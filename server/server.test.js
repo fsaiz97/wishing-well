6f8ce4ab955ad631ee09005d25c2338252b6e087
fsaiz97 <felixsaiz97@gmail.com>
2022-09-27 14:50:48 +0100

    First commit Setup basic server

---
+++ server/server.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const { app } = require("./app");
+
+describe("server tests", () => {
+    it("Should respond to get request at root", async () => {
+        const response = await request(app).get("/");
+        expect(response.statusCode).toBe(200);
+    })
+})
